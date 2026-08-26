aecc240aa5239c9e68181952fd52fc68b306735d
Sofia <sofiabns06@gmail.com>
2026-08-26 18:29:42 -0300

    Bloqueio de publicação por falha de qualidade

--- src/govinsight/quality/__init__.py
+++ src/govinsight/quality/__init__.py
@@ -1,4 +1,10 @@
 from .models import DataQualityRunResult, QualityRunStatus
 from .scoring import calculate_quality_score
+from .service import DataQualityService
 
-__all__ = ["DataQualityRunResult", "QualityRunStatus", "calculate_quality_score"]
+__all__ = [
+    "DataQualityRunResult",
+    "DataQualityService",
+    "QualityRunStatus",
+    "calculate_quality_score",
+]
